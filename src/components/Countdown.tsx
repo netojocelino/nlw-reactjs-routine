109ad2a1a295668c46acdc0010ea27077408a506
Jocelino Neto <netojocelino@gmail.com>
2021-03-03 22:38:05 -0300

    refact: Countdown moved to Context

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,20 +1,18 @@
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 
-import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 
 import styles from '../styles/components/Countdown.module.css';
 
-let countdownTimeout: NodeJS.Timeout;
-
 export function Countdown () {
-    const { startNewChallenge } = useContext(ChallengesContext);
-
-    const [time, setTime] = useState((25 * 60));
-    const [isActive, setIsActive] = useState(false);
-    const [hasFinished, setHasFinished] = useState(false);
-
-    const minutes = Math.floor(time/60);
-    const seconds = Math.floor(time%60)
+    const {
+        minutes,
+        seconds,
+        hasFinished,
+        isActive,
+        startCountdown,
+        resetCountdown
+    } = useContext(CountdownContext);
 
     const [minuteLeft, minuteRight] = String(minutes)
         .padStart(2, '0')
@@ -23,30 +21,7 @@ export function Countdown () {
     const [secondLeft, secondRight] = String(seconds)
         .padStart(2, '0')
         .split('');
-
-
-    function startCountdown() {
-        setIsActive(true);
-    }
-    function resetCountdown() {
-        clearTimeout(countdownTimeout);
-        setIsActive(false);
-        setTime((25 * 60));
-    }
-
-    useEffect(function() {
-        if(isActive && time > 0) {
-            countdownTimeout = setTimeout(function(){
-                setTime(function(previousTime){
-                    return previousTime - 1;
-                });
-            }, 1000);
-        }else if(isActive && time == 0) {
-            setHasFinished(true);
-            setIsActive(false);
-            startNewChallenge();
-        }
-    }, [isActive, time]);
+    
     return (
         <div>
             <div className={styles.countdownContainer}>
